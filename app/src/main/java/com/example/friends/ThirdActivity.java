c7c23036c2eac0c3ea0ba81aad290ddd7883d860
IvanchenkoIR <Iir163@yandex.ru>
2021-11-17 17:12:29 +0400

    3 страницы + переходы

--- app/src/main/java/com/example/friends/ThirdActivity.java
+++ app/src/main/java/com/example/friends/ThirdActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
     }
     public void onClick(View view)
     {
-        Intent intent = new Intent(this, ThirdActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
